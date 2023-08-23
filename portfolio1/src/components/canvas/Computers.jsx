67bbf456d25c41cf16990287d84f41fce53ba4b1
iamlydial <lydia.lavecchia@gmail.com>
2023-08-23 10:55:18 +0200

    added mobile responsiveness to the 3d model of the hero section

--- portfolio1/src/components/canvas/Computers.jsx
+++ portfolio1/src/components/canvas/Computers.jsx
@@ -3,8 +3,9 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
+
   return (
     <>
       <mesh>
@@ -20,8 +21,8 @@ const Computers = () => {
         />
         <primitive
           object={computer.scene}
-          scale={0.75}
-          position={[0, -3.25, -1.5]}
+          scale={isMobile ? 0.7 : 0.75}
+          position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
           rotation={[-0.01, -0.2, -0.1]}
         />
       </mesh>
@@ -30,6 +31,33 @@ const Computers = () => {
 };
 
 const ComputersCanvas = () => {
+  // Define a state variable named 'isMobile' and a function to update it named 'setIsMobile'
+  const [isMobile, setIsMobile] = useState(false);
+
+  // Use the useEffect hook to perform side effects in functional components
+  useEffect(() => {
+    // Create a media query that checks if the screen width is at most 500px
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the 'isMobile' state variable based on the media query result
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      // Update the 'isMobile' state variable based on the new media query result
+      setIsMobile(event.matches);
+    };
+
+    // Add the 'handleMediaQueryChange' callback as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Clean up function: this function will be called when the component is unmounted
+    return () => {
+      // Remove the 'handleMediaQueryChange' listener when the component is unmounted
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []); // An empty dependency array means this effect runs only after the initial render
+
   return (
     <>
       <Canvas
@@ -44,7 +72,7 @@ const ComputersCanvas = () => {
             maxPolarAngle={Math.PI / 2}
             minPolarAngle={Math.PI / 2}
           />
-          <Computers />
+          <Computers isMobile={isMobile} />
         </Suspense>
         <Preload all />
       </Canvas>
